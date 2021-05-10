4050f9400a5a599ed4297e6d94f47ec7760a009f
Carlos Eduardo <78118717+eduardo92005-debug@users.noreply.github.com>
2021-05-10 02:52:42 -0300

    Atomo computacional
    Consiste num algoritmo de estrutura de dados que é bioinspirado na estrutura atômica, e utiliza-se de camadas que são 'gerenciadas' por energias e as 'soluções' que são os eletrons que orbitam uma 'solução primária' que é representada pelos prótons e neutrons.

---
+++ ED - Atomo_computacional/atom.py
@@ -0,0 +1,9 @@
+lista_teste = [x for x in range(0,1000)]
+
+def atom(lista, energia, qntd_camadas):
+    lista = []
+    energia = 0
+    qntd_camadas = 1
+    tam_camadas = len(lista[-1])
+    lista1 =
+
